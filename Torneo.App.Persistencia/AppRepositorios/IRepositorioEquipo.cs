326fe3a1b49bd38630c32c2e73f58a8d27f092ff
Daniel Alejandro Sanchez Paez <programazion1@gmail.com>
2022-09-04 10:31:51 -0500

    Se añade IRepositorioEquipo y RepositorioEquipo

---
+++ Torneo.App.Persistencia/AppRepositorios/IRepositorioEquipo.cs
@@ -0,0 +1,10 @@
+using Torneo.App.Dominio;///Se añade IrepositorioEquipo
+namespace Torneo.App.Persistencia
+{
+    public interface IRepositorioEquipo
+    {
+        public Equipo AddEquipo(Equipo equipo, int idMunicipio, int idDT);
+
+        
+    }
+}
